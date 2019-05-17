ee159136cc73e85c1c04ba3d7dc80379290d97c6
Dmitrii Smirnov <dmitrii.smirnov@phystech.edu>
2019-05-17 20:55:06 +0300

    Now it is possible to install the package locally via pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyCardiac',
+      version='0.1',
+      description='Basic toolkits to process Action Potential signals',
+      url='https://github.com/humanphysiologylab/pyCardiac',
+      author='Human Physiology Lab',
+      author_email='dmitrii.smirnov@phystech.edu',
+      license='MIT',
+      packages=['pyCardiac'],
+      zip_safe=False)
