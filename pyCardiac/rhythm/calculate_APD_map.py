bd29997d1fb172af565c25956a8bc27c78e3dc81
Pofigt <pikunov@phystech.edu>
2019-05-18 19:56:45 +0300

    Small fix with alternance and apd maps.

--- pyCardiac/rhythm/calculate_APD_map.py
+++ pyCardiac/rhythm/calculate_APD_map.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ..signal.analysis import calculate_APD
 
-def calculate_APD_map(data, t_start, t_end):
+def calculate_APD_map(data, t_start, t_end, percentage = 80.):
     time = np.arange(t_start, t_end)
-    apd_map = np.apply_along_axis(lambda x: calculate_APD(time, x[t_start: t_end]), -1, data)
+    apd_map = np.apply_along_axis(lambda x: calculate_APD(time, x[t_start: t_end], percentage = 80.), -1, data)
     return apd_map
--- pyCardiac/rhythm/calculate_alternance_map.py
+++ pyCardiac/rhythm/calculate_alternance_map.py
@@ -2,7 +2,7 @@ import numpy as np
 from ..signal.analysis import calculate_alternance
 
 
-def calculate_alternance_map(data, t_start, t_end, apd_min = 10.):
+def calculate_alternance_map(data, t_start, t_end, percentage = 80., apd_min = 10.):
     time = np.arange(t_start, t_end)
-    alt_map = np.apply_along_axis(lambda x: calculate_alternance(time, x[t_start: t_end], apd_min), -1, data)
+    alt_map = np.apply_along_axis(lambda x: calculate_alternance(time, x[t_start: t_end], percentage, apd_min), -1, data)
     return alt_map
